0508d34a5fce7d6ad29f55102f55df291cd23bdf
vahan akopjanian <vahan.bclub@gmail.com>
2020-06-11 15:37:39 +0300

    added addToCard and handleDetail functions

--- src/context.js
+++ src/context.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { storeProducts, detailProduct } from "./data";
+import { ThemeProvider } from "styled-components";
 
 const ProductContext = React.createContext();
 //Provider
@@ -10,6 +11,9 @@ class ProductProvider extends Component {
   state = {
     products: [],
     detailProduct: detailProduct,
+    cart: [],
+    modalOpen: true,
+    modalProduct: detailProduct,
   };
 
   componentDidMount() {
@@ -27,12 +31,48 @@ class ProductProvider extends Component {
     });
   };
 
-  handleDetail = () => {
-    console.log("hello");
+  getItem = (id) => {
+    const product = this.state.products.find((item) => item.id === id);
+    return product;
+  };
+
+  handleDetail = (id) => {
+    const product = this.getItem(id);
+    this.setState(() => {
+      return { detailProduct: product };
+    });
   };
 
   addToCart = (id) => {
-    console.log(id);
+    let tempProducts = [...this.state.products];
+    const index = tempProducts.indexOf(this.getItem(id));
+    const product = tempProducts[index];
+    product.inCart = true;
+    product.count = 1;
+    const price = product.price;
+    product.total = price;
+
+    this.setState(
+      () => {
+        return { products: tempProducts, cart: [...this.state.cart, product] };
+      },
+      () => {
+        console.log(this.state);
+      }
+    );
+  };
+
+  openModal = (id) => {
+    const product = this.getItem(id);
+    this.setState(() => {
+      return { modalProduct: product, modelOpen: true };
+    });
+  };
+
+  closeModal = (id) => {
+    this.setState(() => {
+      return { modalOpen: false };
+    });
   };
 
   render() {
