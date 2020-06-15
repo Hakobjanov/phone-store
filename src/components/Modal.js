33d72b8d83d079fe76bc1bacd14acee9737031b9
vahan akopjanian <vahan.bclub@gmail.com>
2020-06-15 16:25:43 +0300

    setting up modal

---
+++ src/components/Modal.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Modal extends Component {
+  render() {
+    return (
+      <div>
+        <h1> modal is this</h1>
+      </div>
+    );
+  }
+}
