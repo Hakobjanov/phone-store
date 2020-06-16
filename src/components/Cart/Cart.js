b0665150e84ac48d9e353ae7e8cd95a6bab03b3e
vahan akopjanian <vahan.bclub@gmail.com>
2020-06-16 18:45:32 +0300

    adding cart counter and totals

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,11 +1,32 @@
 import React, { Component } from "react";
 import Title from "../Title";
+import CartColums from "./CartColums.js";
+import EmptyCart from "./EmptyCart";
+import CartList from "./CartList";
+import CartTotals from "./CartTotals";
+import { ProductConsumer } from "../../context";
 
 export default class Cart extends Component {
   render() {
     return (
       <section>
-        <Title name="your" title="cart" />
+        <ProductConsumer>
+          {(value) => {
+            const { cart } = value;
+            if (cart.length > 0) {
+              return (
+                <React.Fragment>
+                  <Title name="your" title="cart" />
+                  <CartColums />
+                  <CartList value={value} />
+                  <CartTotals value={value} />
+                </React.Fragment>
+              );
+            } else {
+              return <EmptyCart />;
+            }
+          }}
+        </ProductConsumer>
       </section>
     );
   }
